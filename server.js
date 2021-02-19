61dc229d2ed7dbe3ac436f8793743c01c92f6ddd
brittanymbui <brittymbui@gmail.com>
2021-02-19 09:56:03 -0800

    First commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>GameLib API</h1>')
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at ${PORT}`);
+});
